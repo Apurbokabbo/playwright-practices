f6a38b4133c9f41d8d6563ccb61a3950a4ed8063
Apurbokabbo <apurbokabbo1999@gmail.com>
2025-07-16 18:51:25 +0600

    screenshot and page tittle testcases added

--- src/test/java/Testcases/install_run_playwright.java
+++ src/test/java/Testcases/install_run_playwright.java
@@ -1,12 +1,13 @@
 package Testcases;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 import org.testng.annotations.Test;
 
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class install_run_playwright {
 
@@ -26,12 +27,43 @@ public class install_run_playwright {
     @Test
     public void takeScreenShot() {
         Playwright playwright = Playwright.create();
-            Browser browser = playwright.webkit().launch();
-            new BrowserType.LaunchOptions().setHeadless(false);
+        Browser browser = playwright.webkit().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://www.saucedemo.com/v1/");
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    public void assertion() {
+            Playwright playwright = Playwright.create();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
-            page.navigate("https://www.saucedemo.com/v1/");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
-        }
+            page.navigate("https://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+
+            // create a locator
+            Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+
+            // Expect an attribute "to be strictly equal" to the value.
+            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+
+            // Click the get started link.
+            getStarted.click();
+
+            // Expects page to have a heading with the name of Installation.
+            assertThat(page.getByRole(AriaRole.HEADING,
+                    new Page.GetByRoleOptions().setName("Installation"))).isVisible();
+
+        browser.close();
+        playwright.close();
+    }
+
 
 
 
